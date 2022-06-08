96a4987a3f0c3eaa571851c7ba7a823b38b2bc1b
lior2911 <aytegev1@gmail.com>
2022-06-08 09:02:02 +0300

    2cmt

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
-function printTimer(){
-  
-}
+// function printTimer(){
+// setInterval(()=>{
+//   document.write("timer")
+// },4000)
+// }
+// printTimer()
+
+// var counter = 0;
+// function printCounter(){
+//   setInterval(()=>{
+//     document.write(counter++)
+//   },6000)
+// }
+// printCounter()\
+
+// var intervalId = 0 ;
+// var h2_ = document.createElement("h2")
+// document.body.append(h2_)
+// function printTimer2(){
+//  intervalId =  setInterval(()=>{
+//  h2_.innerText+= "i love timers"
+//   },5000)
+// }
+// printTimer2()
+
+// function stopInterval(){
+//   clearInterval(intervalId)
+// }
+
+
+// var todayDate = new Date();
+// var nowHour = todayDate.getHours()+":"+todayDate.getMinutes()+":"+todayDate.getSeconds()+"\n";
+// function getTime(){
+//   setInterval(()=>{
+//     h2_.innerText += nowHour
+//   },1000)
+// }
+// getTime()
+

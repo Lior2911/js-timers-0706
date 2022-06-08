e91298179817e262867b64c151c605ce8749c757
lior2911 <aytegev1@gmail.com>
2022-06-08 13:31:32 +0300

    3cmt

--- main.js
+++ main.js
@@ -37,3 +37,39 @@
 // }
 // getTime()
 
+var intervalId;
+var time =0;
+
+function printTime(){
+  var myTime = new Date();
+
+  var secondsFromDate = myTime.getSeconds();
+  var secondsFromInput = parseInt(timeInput.value.substr(6))
+
+  var minFromDate = myTime.getMinutes();
+  var minFromInput = parseInt(timeInput.value.substr(3,2))
+
+  var hourFromDate = myTime.getHours();
+  var hourFromInput = parseInt(timeInput.value.substr(0,2))
+  intervalId =setInterval(()=>{
+    timeElement.innerHTML=
+    hourFromDate+":"+minFromDate +":"+secondsFromDate++;
+    if(secondsFromDate==60){
+      secondsFromDate = 0;
+      minFromDate ++;
+      if(minFromDate == 60){
+        minFromDate  = 0;
+        hourFromDate++
+      }
+      if(hourFromDate==24){
+        hourFromDate=0;
+      }
+    }
+    if(myTime.getTime()==myTime.setHours(hourFromInput,minFromInput,secondsFromInput)){
+      clearInterval(intervalId)
+    }
+  },1000)
+
+
+
+}
